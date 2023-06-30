412173074147f9a07edb41af56e68c95a40253b0
xingran <xingran@zazl.cc>
2023-06-30 20:54:46 +0800

    Update

--- Assets/2D RPG Kit/Scripts/PropertiesActivator.cs
+++ Assets/2D RPG Kit/Scripts/PropertiesActivator.cs
@@ -1,34 +1,34 @@
-using System;
+using System.Collections.Generic;
 using UnityEngine;
+
 
 
 [RequireComponent(typeof(BoxCollider2D))]
 public class PropertiesActivator : MonoBehaviour
 {
+    private Sprite[] _sprites;
     [SerializeField] 
-    private CharacterProperties characterProperties;
-    
+    private List<CharacterProperties> characterProperties;
     [SerializeField]
-    private bool onlyActiveOnce = true;
+    public List<string[]> dialogues; // 每个string数组代表一个对话，可以在Unity编辑器中设置
 
-    private bool triggered = false;
-    private void OnTriggerEnter2D(Collider2D other)
+
+    private int _curIndex;
+    private CharacterProperties _curCharacterProperties;
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(other.tag == "Player")
+        if (collision.gameObject.CompareTag("Player")) // 确保只有玩家可以触发
         {
-            if (onlyActiveOnce)
-            {
-                if (!triggered)
-                {
-                    AddProperties();
-                    triggered = true;
-                }
-            }
-            else
-            {
-                AddProperties();
-                triggered = true;
-            }
+            // 在所有对话中随机选择一个
+            _curIndex = UnityEngine.Random.Range(0, dialogues.Count);
+            _curCharacterProperties = characterProperties[_curIndex];
+            string[] selectedDialogue = dialogues[_curIndex];
+            
+            // 开始对话
+            DialogManager.instance.ShowDialogAuto(_sprites,selectedDialogue,false);
+            AddProperties();
         }
     }
 
@@ -39,10 +39,10 @@ public class PropertiesActivator : MonoBehaviour
             return;
         }
         
-        PlayerController.instance.characterProperties.AddOperation(characterProperties);
+        PlayerController.instance.characterProperties.AddOperation(_curCharacterProperties);
         if (PlayerController.instance.OnPropertiesChangeEvent != null)
         {
-            PlayerController.instance.OnPropertiesChangeEvent(characterProperties);
+            PlayerController.instance.OnPropertiesChangeEvent(_curCharacterProperties);
         }
     }
 
