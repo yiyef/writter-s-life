a738d104325910559ad4e6c72942ebf00cf12175
xingran <xingran@zazl.cc>
2023-06-30 20:37:51 +0800

    Update

--- Assets/2D RPG Kit/Scripts/CharacterProperties.cs
+++ Assets/2D RPG Kit/Scripts/CharacterProperties.cs
@@ -1,9 +1,32 @@
 using UnityEngine;
 
-namespace Game.Scripts.GameEngine.Editor
+
+public class CharacterProperties : MonoBehaviour
 {
-    public class CharacterProperties : MonoBehaviour
+    public int Time;
+    public int CurrentHour;
+    public int CurrentMinute;
+
+    public int Stamina;
+    public int Spirit;
+    public int Food;
+    public int Money;
+    public int Day;
+
+    public void AddOperation(CharacterProperties add)
     {
-        
+        if (add == null)
+        {
+            return;
+        }
+
+        this.Time = add.Time;
+        this.CurrentHour = add.CurrentHour;
+        this.CurrentMinute = add.CurrentMinute;
+        this.Stamina = add.Stamina;
+        this.Spirit = add.Spirit;
+        this.Food = add.Food;
+        this.Money = add.Money;
+        this.Day = add.Day;
     }
 }
--- Assets/2D RPG Kit/Scripts/PropertiesActivator.cs
+++ Assets/2D RPG Kit/Scripts/PropertiesActivator.cs
@@ -2,35 +2,6 @@ using System;
 using UnityEngine;
 
 
-public class CharacterProperties : MonoBehaviour
-{
-    public int Time;
-    public int CurrentHour;
-    public int CurrentMinute;
-
-    public int Stamina;
-    public int Spirit;
-    public int Food;
-    public int Money;
-    public int Day;
-
-    public void AddOperation(CharacterProperties add)
-    {
-        if (add == null)
-        {
-            return;
-        }
-        this.Time = add.Time;
-        this.CurrentHour = add.CurrentHour;
-        this.CurrentMinute = add.CurrentMinute;
-        this.Stamina = add.Stamina;
-        this.Spirit = add.Spirit;
-        this.Food = add.Food;
-        this.Money = add.Money;
-        this.Day = add.Day;
-    }
-}
-
 [RequireComponent(typeof(BoxCollider2D))]
 public class PropertiesActivator : MonoBehaviour
 {
